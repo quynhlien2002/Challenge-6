bb38ad0a04ebd9450c2e93a900f399b990f58b74
Daisy Lien <daisylien1211@gmail.com>
2022-08-09 16:06:56 -0700

    change

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
-var citySearch = $('.city-search');
-var button = $('.btn');
-var cityName = $('.card-title');
-var temp = $('#temp');
-var wind = $('#wind');
-var humidity = $('#humidity');
-var uv = $('#uv');
+var citySearch = document.querySelector(".city-search");
+var button = document.querySelector(".btn");
+var cityName = document.querySelector("card-title");
+var temp = document.getElementById("temp");
+var wind = document.getElementById("wind");
+var humidity = document.getElementById("humidity");
+var uv = document.getElementById("uv");
 
 button.addEventListener('click', function(){
-    fetch ('http://api.openweathermap.org/geo/1.0/direct?q='+citySearch.value+'&appid=636f19696100c90191a9c3a1c1db82f4')
+    fetch ('https://api.openweathermap.org/data/2.5/weather?q='+citySearch.value+'&appid=636f19696100c90191a9c3a1c1db82f4')
     .then(function (response) {
         return response.json();
     })
