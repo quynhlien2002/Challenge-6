0fcd5b1cbd2c3bf5bf0a0e28ebd04b6b49b98028
Daisy Lien <daisylien1211@gmail.com>
2022-08-09 17:41:42 -0700

    change

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
-var citySearch = document.querySelector(".city-search");
+var citySearch = document.querySelector(".form-control");
 var button = document.querySelector(".btn");
-var cityName = document.querySelector("card-title");
+var cityName = document.getElementById("city-name");
 var temp = document.getElementById("temp");
 var wind = document.getElementById("wind");
 var humidity = document.getElementById("humidity");
 var uv = document.getElementById("uv");
+var today = moment().format('MMMM Do YYYY, h:mm:ss a');
+
 
 button.addEventListener('click', function(){
     fetch ('https://api.openweathermap.org/data/2.5/weather?q='+citySearch.value+'&appid=636f19696100c90191a9c3a1c1db82f4')
@@ -12,9 +14,36 @@ button.addEventListener('click', function(){
         return response.json();
     })
     .then(function (data){
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var windValue = data['wind']['speed'];
+        var humidityValue = data['main']['humidity'];
+        var lat = data['coord']['lat'];
+        var lon = data ['coord']['lon'];
         console.log(data);
+
+        cityName.innerHTML = nameValue + " " + " " + today;
+        temp.innerHTML = "Temp: " + tempValue + " Farenheit";
+        wind.innerHTML = "Wind: " + windValue + " MPH";
+        humidity.innerHTML = "Humidity: " + humidityValue + "%";
+
+        fetch('http://api.openweathermap.org/data/2.5/uvi?appid=636f19696100c90191a9c3a1c1db82f4&lat='+lat+'&lon='+lon)
+    .then (function(response){
+        return response.json();
+    })
+    .then(function (data){
+        var uvValue = data['uvi'];
+        console.log(uv)
+        console.log(uvValue)
+        uv.innerHTML = uvValue;
+    })
+    
     });
+
+    
 });
+
+
 
 
 
